ae4db31732fc46ba24e8681f5607430fb0805707
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-05-02 17:29:06 -0700

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import Layout from '../components/layout'
+
+const NotFoundPage = () => (
+  <Container>
+    <Layout>
+      <h1>Fuggedaboutit!</h1>
+      <p>The page you're looking for is sleeping with the fishes.</p>
+    </Layout>
+  </Container>
+)
+
+export default NotFoundPage
