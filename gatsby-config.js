e961eea819b907ba106bcfe28938779a43e37eef
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-05-02 17:15:38 -0700

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: 'MJPeppers.dev',
+    author: 'Michael J Peppers <mjpeppersdev@protonmail.com>',
+    authorLink: 'https://github.com/MJPeppersdev',
+    disqus: false
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/examples`,
+        name: 'examples'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files'
+        ]
+      }
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        includePaths: [`${__dirname}/node_modules`, `${__dirname}/src/`],
+        precision: 8
+      }
+    }
+  ]
+}
